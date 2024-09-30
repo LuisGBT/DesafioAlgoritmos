22574981b4088b849c26c1bd3b41adca4ce91bce
LuisGBT <luizingustavo507@gmail.com>
2024-09-29 21:52:01 -0300

    att

--- src/ex03_Lista01.java
+++
@@ -1,9 +0,0 @@
-public class ex03_Lista01 {
-    public static void main(String[] args) {
-        //Calcular a área de um retângulo
-        double base = 10;
-        double altura = 20;
-
-        System.out.println("A area do triangulo é " + base * altura);
-    }
-}
